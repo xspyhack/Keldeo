599c60dc2dd074ad04b8bccc3ad2ea9d276bae9d
xspyhack <xspyhack@gmail.com>
2018-08-25 15:49:31 +0800

    Add example project

---
+++ Alligator/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Alligator
+//
+//  Created by bl4ckra1sond3tre on 2018/8/25.
+//  Copyright © 2018 blessingsoftware. All rights reserved.
+//
+
+import UIKit
+import Logger
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+
+        Log.d("Do any additional setup after loading the view, typically from a nib. This should't be log to file.")
+
+        Log.i("View did load")
+
+        Log.e("This is Log.e() message synchronously")
+        print("This is print() message")
+
+        NSLog("This is NSLog() message")
+    }
+
+}
+
